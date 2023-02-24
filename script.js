15142c5c277e31884b887a0bc56716378d3b75d2
Christine Ramm <chri42mp@stud.kea.dk>
2023-02-24 14:49:51 +0100

    start on styling + typewriter

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 "use strict";
 
+const str = document.querySelector("#typewriter").textContent;
+document.querySelector("#typewriter").textContent = "";
+let iterator = 0;
+let maxNumberOfIterations;
+
+initLoop();
+
+function initLoop() {
+  maxNumberOfIterations = str.length;
+  loop();
+}
+
+function loop() {
+  document.querySelector("#typewriter").textContent += str[iterator];
+  iterator++;
+  if (iterator < maxNumberOfIterations) {
+    setTimeout(loop, 100);
+  }
+}
+
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
